18ce46b3eb3e13a0deeb589b4806e469d2074511
윤진호 <54177070+KR-EGOIST@users.noreply.github.com>
2024-04-14 20:47:49 +0900

    Create 특정 문자 제거하기.js

---
+++ 특정 문자 제거하기.js
@@ -0,0 +1,8 @@
+// function solution(my_string, letter) {
+//   return my_string.replaceAll(letter, '');
+// }
+
+// function solution(my_string, letter) {
+//   const answer = my_string.split(letter).join('');
+//   return answer;
+// }
