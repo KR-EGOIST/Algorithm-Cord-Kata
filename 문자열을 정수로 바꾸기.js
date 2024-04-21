186657c07872b7e10bf0b4960b947d981359bfd2
윤진호 <54177070+KR-EGOIST@users.noreply.github.com>
2024-04-22 08:36:41 +0900

    Create 문자열을 정수로 바꾸기.js

---
+++ 문자열을 정수로 바꾸기.js
@@ -0,0 +1,19 @@
+/*
+문자를 숫자로 바꾸기
+Number(값) / parseInt(값)
+*/
+
+function solution(s) {
+  return Number(s);
+}
+
+let result = solution('-1234');
+console.log(result);
+
+function strToInt(str) {
+  return str / 1;
+}
+
+function strToInt(str) {
+  return +str;
+}
