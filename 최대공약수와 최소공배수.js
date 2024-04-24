fbabe3c94728aa28cf65101ab406a43cbcb08098
윤진호 <54177070+KR-EGOIST@users.noreply.github.com>
2024-04-24 17:38:33 +0900

    Create 최대공약수와 최소공배수.js

---
+++ 최대공약수와 최소공배수.js
@@ -0,0 +1,18 @@
+/*
+<유클리드 호제법>
+
+최대공약수 (GCD)
+2개의 자연수 a, b(a > b)에 대해서 a를 b로 나눈 나머지가 r일 때, a와 b의 최대공약수는 b와 r의 최대공약수와 같다.
+
+최소공배수 (LCM)
+두 수 a와 b의 최소공배수는 a와 b의 곱을 a와 b의 최대공약수를 나눈 것과 같다.
+*/
+
+function solution(n, m) {
+  const gcd = (a, b) => (a % b === 0 ? b : gcd(b, a % b));
+  const lcm = (a, b) => (a * b) / gcd(a, b);
+  return [gcd(n, m), lcm(n, m)];
+}
+
+let result = solution(3, 12);
+console.log(result);
