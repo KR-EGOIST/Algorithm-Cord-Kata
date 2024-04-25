42749f325373cc5a909039e2d934ffebb6727dd5
윤진호 <54177070+KR-EGOIST@users.noreply.github.com>
2024-04-25 12:44:42 +0900

    Create 삼총사.js

---
+++ 삼총사.js
@@ -0,0 +1,36 @@
+function solution(number) {
+  let cnt = 0;
+
+  for (let i = 0; i < number.length; i++) {
+    for (let j = i + 1; j < number.length; j++) {
+      for (let k = j + 1; k < number.length; k++) {
+        if (number[i] + number[j] + number[k] === 0) cnt++;
+      }
+    }
+  }
+
+  return cnt;
+}
+
+let result = solution([-3, -2, -1, 0, 1, 2, 3]);
+console.log(result);
+
+/*
+재귀 함수 이용 구현
+*/
+function solution(number) {
+  let result = 0;
+
+  const combination = (current, start) => {
+    if (current.length === 3) {
+      result += current.reduce((acc, cur) => acc + cur, 0) === 0 ? 1 : 0;
+      return;
+    }
+
+    for (let i = start; i < number.length; i++) {
+      combination([...current, number[i]], i + 1);
+    }
+  };
+  combination([], 0);
+  return result;
+}
