176f35d04635d80d6128ab2de6505d8ceab9ed34
윤진호 <54177070+KR-EGOIST@users.noreply.github.com>
2024-05-08 10:12:09 +0900

    Create 옹알이 (2).js

---
+++ 옹알이 (2).js
@@ -0,0 +1,62 @@
+function solution(babbling) {
+  const can = ['aya', 'ye', 'woo', 'ma'];
+
+  return babbling.reduce((possible, babble) => {
+    // 2번 연속된 옹알이
+    // 연속해서 같은 발음을 하는 것을 어려워합니다.
+    for (let i = 0; i < can.length; i += 1) {
+      if (babble.includes(can[i].repeat(2))) return possible;
+    }
+
+    // 섞여서한 옹알이
+    // 네 가지 발음을 조합해서 만들 수 있는 발음밖에 하지 못하고
+    for (let i = 0; i < can.length; i += 1) {
+      console.log(babble.split(can[i]));
+      babble = babble.split(can[i]).join(' ').trim();
+    }
+
+    // 한번한 옹알이
+    // "aya", "ye", "woo", "ma" 네 가지 발음
+    if (babble) return possible;
+
+    return (possible += 1);
+  }, 0);
+}
+
+let result = solution(['ayaye', 'uuu', 'yeye', 'yemawoo', 'ayaayaa']);
+console.log(result);
+
+//=============================================================
+
+function solution(babbling) {
+  const regexp1 = /(aya|ye|woo|ma)\1+/;
+  const regexp2 = /^(aya|ye|woo|ma)+$/;
+
+  return babbling.reduce(
+    (ans, word) => (!regexp1.test(word) && regexp2.test(word) ? ++ans : ans),
+    0
+  );
+}
+
+function solution(babbling) {
+  const can = ['aya', 'ye', 'woo', 'ma'];
+  let count = 0;
+
+  for (let i = 0; i < babbling.length; i++) {
+    let babble = babbling[i];
+
+    for (let j = 0; j < can.length; j++) {
+      if (babble.includes(can[j].repeat(2))) {
+        break;
+      }
+
+      babble = babble.split(can[j]).join(' ');
+    }
+
+    if (babble.split(' ').join('').length === 0) {
+      count += 1;
+    }
+  }
+
+  return count;
+}
