0e04ff7583993237113d3ff999b0300333c80dc0
윤진호 <54177070+KR-EGOIST@users.noreply.github.com>
2024-05-09 19:41:51 +0900

    Update 체육복.js

--- 체육복.js
+++ 체육복.js
@@ -86,3 +86,35 @@ function solution(n, lost, reserve) {
   }
   return answer;
 }
+
+//====================================================================
+
+function solution(n, lost, reserve) {
+    var answer = 0;
+    let uniform = new Array(n).fill(1);
+    
+    for(let i=0; i<reserve.length; i++){
+        uniform[reserve[i]-1]++;
+    }
+    
+    for(let i=0; i<lost.length; i++){
+        uniform[lost[i]-1]--;
+    }
+    
+    for(let i=0; i<n; i++){
+        if(uniform[i] === 0 && i>0 && uniform[i-1] >1){
+            uniform[i]++;
+            uniform[i-1]--;
+        }else if(uniform[i] ===0 && i<n - 1 && uniform[i+1]>1){
+            uniform[i]++;
+            uniform[i+1]--;
+        }
+    }
+    
+    for(let i=0; i<n; i++){
+        if(uniform[i]>0){
+            answer++
+        }
+    }
+    return answer;
+}
