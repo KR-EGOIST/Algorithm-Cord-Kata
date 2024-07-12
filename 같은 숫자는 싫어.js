f58e43915d1fc016375d818cc4cde23369c027bb
윤진호 <54177070+KR-EGOIST@users.noreply.github.com>
2024-07-12 09:30:01 +0900

    Update 같은 숫자는 싫어.js

--- 같은 숫자는 싫어.js
+++ 같은 숫자는 싫어.js
@@ -2,7 +2,7 @@ function solution(arr) {
   const stack = [];
 
   // 마지막 값이 undefined 가 나오는데 마지막 숫자를 스택에 넣기 위해서
-  // i를 0부터 arr.length 까지 순회
+  // i를 0부터 arr.length - 1 까지 순회
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] !== arr[i + 1]) {
       stack.push(arr[i]);
